4e4cbf950ebf6bafd3870d6dcb67c8caa436b644
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-19 17:34:07 +0330

    Added getter setter and args constructor to Address Embeddable class

--- src/main/java/com/delivery/order/Address.java
+++ src/main/java/com/delivery/order/Address.java
@@ -3,7 +3,16 @@ package com.delivery.order;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class Address {
     @NotBlank
