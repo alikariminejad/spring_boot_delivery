c87bd782af568f90b29634ce0e85b5f6f3b15b6e
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-08-19 00:05:50 +0330

    Changed the dependencies of pom.xml and commentd the tests

--- notification-service/src/test/java/com/delivery/notification/TestNotificationServiceApplication.java
+++ notification-service/src/test/java/com/delivery/notification/TestNotificationServiceApplication.java
@@ -2,10 +2,10 @@ package com.delivery.notification;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestNotificationServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.from(NotificationServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
-}
+//public class TestNotificationServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.from(NotificationServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+//	}
+//
+//}
